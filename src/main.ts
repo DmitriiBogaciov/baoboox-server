84e3dd978a975ef62356ae5ae5cb0400503765f8
Dmitrii Bogaciov <dmitrii.bogaciov@gmail.com>
2025-11-05 03:55:25 +0100

    import update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import { Server } from 'http';
 
 let server: Server;
